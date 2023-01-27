3f937603df0ba1c175eaa8748fab78c2999fae19
Matt Curtis <physicallyrendered@gmail.com>
2023-01-26 21:41:24 -0500

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 voice=$1
 quality=$2
 python3 tortoise/read.py --textfile input/input.txt --voice $voice --preset $quality
-mv results/longform/$voice/combined.wav results/longform/combined.wav
+mv -f results/longform/$voice/combined.wav results/longform/combined.wav
